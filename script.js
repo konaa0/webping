cf9df6ec9529934a4e3523c7d35a6a9f4f79351e
vornex <79860992+vornex-gh@users.noreply.github.com>
2023-08-31 19:22:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const downloadButton = document.getElementById("downloadButton");
+
+downloadButton.addEventListener("click", () => {
+    // Add your download logic here
+    alert("Download started!");
+});
